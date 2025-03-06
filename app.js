1d99ada81ca302a61452fbd2f7fb635ce26ac340
viniitl <marcosvmaguiar@gmail.com>
2025-03-06 20:13:32 -0300

    Emplementa lista de amigos

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+
+function adicionarAmigo(){
+    let campoEntrada = document.getElementById("amigo");
+    let nome = campoEntrada.value.trim();
+
+    if(nome === ""){
+        alert("Por favor, insira um nome");
+        return;
+    }
+    amigos.push(nome);
+    console.log("Lista de Amigos:" , amigos);
+    nome.value = "";
+}
+
